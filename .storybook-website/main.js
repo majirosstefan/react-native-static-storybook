643e8b4bb66f18ff8a382160dc21bdca8c329ac4
stefan-majiros <stefan.majiros>
2020-03-29 21:26:33 +0200

    fix for svg files inside storybook web build

--- .storybook-website/main.js
+++ .storybook-website/main.js
@@ -1,23 +1,30 @@
 module.exports = {
   stories: ['../storybook/stories/index.js'],
   webpackFinal: async config => {
-    console.log('Using CUSTOM CONFIG');
-
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
       'react-native$': 'react-native-web',
-      // make sure we're rendering output using **web** Storybook not react-native
       '@storybook/react-native': '@storybook/react',
-      // plugin-level react-native-web extensions
       'react-native-svg': 'react-native-svg/lib/commonjs/ReactNativeSVG.web',
-      // ...
     };
-    // mutate babel-loader
+
+    // handle svg support
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test.test('.svg'),
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: 'svg-react-loader',
+    });
+
+    console.log(config.module.rules);
+
     config.module.rules[0].use[0].options.plugins.push([
       'react-native-web',
       {commonjs: true},
     ]);
+
     return config;
   },
 };
